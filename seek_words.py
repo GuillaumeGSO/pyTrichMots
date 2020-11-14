9d61620378c46a423426305f6051d1a10d1adaa9
GuillaumeGSO <GuillaumeGSO@users.noreply.github.com>
2020-11-14 15:47:06 +0100

    First commit : with TU and UTF8 assets

---
+++ seek_words.py
@@ -0,0 +1,90 @@
+import csv
+import time
+
+
+def isListEmptyOrFullOfNone(lst):
+    if not lst:
+        #print("List vide ou nulle")
+        return True
+    if all(x is None or not x for x in lst):
+        #print("List avec des None ou "" seulement")
+        return True
+    #print("Liste non vide")
+    return False
+
+
+def isSearchByContent(word, lstCar=[]):
+    """
+    Retourne faux si word est non renseigné
+    Retourne faux si rien dans lstCar
+    Retourne faux si il y moins de caractère dans lstCar que dans word
+    Retourne vrai si chaque caractère de word est présent dans lstCar
+    Retourne 
+    """
+    if word == None or len(word) == 0:
+        return False
+    if isListEmptyOrFullOfNone(lstCar):
+        return False
+    if len(lstCar) < len(word):
+        return False
+    compteur = 0
+    for car in lstCar:
+        #print(f"recherche de {car} dans {word}")
+        if car in word:
+            compteur += 1
+            #print(f"-{car} est dans {word} : count={compteur}")
+        # else:
+            #print(f"-{car} n'est pas dans {word}")
+    #print(f"On a trouvé {compteur} lettres dans {word}")
+    if compteur != len(word):
+        return False
+    return True
+
+
+"""
+Retourne faux si word n'est pas renseigné
+Retourne vrai si pas de Hint renseigné
+"""
+
+
+def isSearchByHint(word, lstHint=[]):
+    if not word:
+        return False
+    if isListEmptyOrFullOfNone(lstHint):
+        return True
+    for idx, lettre in enumerate(lstHint):
+        if lettre:
+            #print(f"recherche de {lettre} à position {idx} dans {word}")
+            if word.find(lettre) != idx:
+                #print(f"abandon car {lettre} n'est pas à {idx} dans {word}")
+                return False
+    return True
+
+
+def searchInFile(nbCar=99, lstCar=[], lstHint=[]):
+    if isListEmptyOrFullOfNone(lstCar) and isListEmptyOrFullOfNone(lstHint):
+        raise Exception(
+            "Les paramètres lstCar et lstHint ne peuvent pas être vides en même temps")
+
+    for line in open("assets/" + str(nbCar) + ".txt", "r"):
+        mot = line.strip()
+        if isSearchByContent(mot, lstCar) and isListEmptyOrFullOfNone(lstHint):
+            yield mot
+        elif isListEmptyOrFullOfNone(lstCar) and isSearchByHint(mot, lstHint):
+            yield mot
+        elif isSearchByContent(mot, lstCar) and isSearchByHint(mot, lstHint):
+            yield mot
+
+
+def searchInManyFiles(lstCar=[], lstHint=[]):
+    for i in reversed(range(1, len(lstCar)+1)):
+        for m in searchInFile(nbCar=i, lstCar=lstCar, lstHint=lstHint):
+            yield m
+
+
+s = time.time()
+print("-".join(searchInManyFiles(lstCar=[
+               'i', 'n', 'f', 'o', 'r', 'm', 'a', 't', 'i', 'q', 'u', 'e'], lstHint=['i', '', '', 'o', None,'m' , ])))
+
+e = time.time()
+print(f"{e-s} secondes")
---
+++ test_seek_words.py
@@ -0,0 +1,164 @@
+import seek_words as sw
+import pytest
+
+"""
+Le fichie 99.txt contient
+abc
+abd
+xyz
+opa
+api
+"""
+
+"""
+isListEmptyOrFullOfNone
+"""
+
+
+def test_isListEmptyOrFullOfNone():
+    with pytest.raises(TypeError):
+        assert sw.isListEmptyOrFullOfNone()
+    assert sw.isListEmptyOrFullOfNone([])
+    assert sw.isListEmptyOrFullOfNone([None, None])
+    assert sw.isListEmptyOrFullOfNone(['', ""])
+    assert sw.isListEmptyOrFullOfNone([None, "", ''])
+
+
+"""
+isSearchByContent
+"""
+
+
+def test_searchByContent_NoWord():
+    # Retourne faux si word est non renseigné
+    assert sw.isSearchByContent("") == False
+    assert sw.isSearchByContent(None) == False
+
+
+def test_searchByContent_NoLst():
+    # Retourne faux si rien dans la lstCar
+    assert sw.isSearchByContent("ab") == False
+    assert sw.isSearchByContent("ab", [None, None]) == False
+
+
+def test_searchByContent_LessCar():
+    # Retourne faux si il y moins de caractère dans lstCar que dans word
+    assert sw.isSearchByContent("ab", ["a"]) == False
+
+
+def test_searchByContent_SameCar():
+    # Retourne vrai si chaque caractère de word est présent dans lstCar
+    assert sw.isSearchByContent("ab", ["b", 'a'])
+
+
+def test_searchByContent_MoreCars():
+    # Retourne vrai si chaque caractère de word est présent dans lstCar
+    assert sw.isSearchByContent("ab", ["b", 'c', 'a', 'r'])
+
+
+"""
+isSearchByHint
+"""
+
+
+def test_isSearchByHint_NoWord():
+    # Retourne faux si word n'est pas renseigné
+    assert sw.isSearchByHint('') == False
+    # Exception si pas de word
+    with pytest.raises(TypeError):
+        assert sw.isSearchByHint()
+
+
+def test_isSearchByHint_NoHint():
+    # Retourne vrai si pas de Hint renseigné
+    assert sw.isSearchByHint('abc')
+    assert sw.isSearchByHint('abc', [None, None, None])
+
+
+def test_isSearchByHint_Hint():
+    # Retourne vrai si pas de Hint renseigné
+    assert sw.isSearchByHint('abc', [None, 'b', ''])
+    assert sw.isSearchByHint('abc', ['', '', 'c'])
+
+
+"""
+searchInFile
+"""
+
+
+def test_recherche_exception():
+    # Exception ValueError si aucune liste fournie
+    with pytest.raises(Exception) as excp:
+        assert sw.searchInFile(nbCar=99)
+        excp.match("Les paramètres lstCar et lstHint ne peuvent pas être vides en même temps")
+
+
+def test_recherche_1_car():
+    result = list(sw.searchInFile(nbCar=99, lstCar=['a']))
+    # Aucun mot de contient toutes les lettres : il n'y en a pas assez
+    assert len(result) == 0
+
+
+def test_recherche_1_car_rien():
+    result = list(sw.searchInFile(nbCar=99, lstCar=['u']))
+    # Aucun mot de contient toutes les lettres
+    assert len(result) == 0
+
+
+def test_recherche_2_cars():
+    result = list(sw.searchInFile(nbCar=99, lstCar=['b', 'a']))
+    # Aucun mot de contient toutes les lettres
+    assert len(result) == 0
+
+
+def test_recherche_3_cars():
+    result = list(sw.searchInFile(nbCar=99, lstCar=['d', 'a', 'b']))
+    # Un seul mot contient toutes les lettres
+    assert len(result) == 1
+    assert "abd" in result
+
+
+def test_recherche_3_cars_et_plus():
+    #On passe l'ensemble des lettres existantes
+    result = list(sw.searchInFile(nbCar=99, lstCar=[
+                  'a', 'b', 'c', 'd', 'i', 'o', 'p', 'x', 'y', 'z']))
+    #On récupère donc l'ensemble des mots
+    assert len(result) == 5
+    assert "abc" in result
+    assert "abd" in result
+    assert "opa" in result
+    assert "api" in result
+    assert "xyz" in result
+
+
+def test_recherche_hint_1():
+    result = list(sw.searchInFile(nbCar=99, lstHint=[None, "p", None]))
+    # Tous les mots qui ont "p" en deuxième position
+    assert len(result) == 2
+    assert "opa" in result
+    assert "api" in result
+
+
+def test_recherche_hint_2():
+    result = list(sw.searchInFile(nbCar=99, lstHint=[None, "y"]))
+    assert len(result) == 1
+    assert "xyz" in result
+
+
+def test_recherche_hint_3():
+    result = list(sw.searchInFile(nbCar=99, lstHint=["a"]))
+    # Tous les mots qui commencent par "a"
+    assert len(result) == 3
+    assert "abc" in result
+    assert "abd" in result
+    assert "api" in result
+
+def test_recherche_complete():
+    #On passe l'ensemble des lettres existantes
+    result = list(sw.searchInFile(nbCar=99, lstCar=[
+                  'a', 'b', 'c', 'd', 'i', 'o', 'p', 'x', 'y', 'z'], lstHint=['a']))
+    #On récupère donc l'ensemble des mots
+    assert len(result) == 3
+    assert "abc" in result
+    assert "abd" in result
+    assert "api" in result
