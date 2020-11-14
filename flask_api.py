3f409af2426388a2c116796fd4283cca59d9755d
GuillaumeGSO <GuillaumeGSO@users.noreply.github.com>
2020-11-15 00:24:13 +0100

    First flask API

---
+++ flask_api.py
@@ -0,0 +1,22 @@
+import flask
+from flask import request, jsonify
+from seek_words import searchInFile
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+
+
+@app.route('/simple', methods=['GET'])
+def simple():
+    nbcar = request.args.get('nbcar')
+    lstcar = request.args.get('lstcar')
+    return jsonify(list(searchInFile(nbcar, lstcar)))
+    """
+    Pour tester
+    http://127.0.0.1:5000/simple?nbcar=9&lstcar=a,b,c,e,r,t,y,u,i,i,f
+    http://127.0.0.1:5000/simple?nbcar=5&lstcar=m,e,r,d,e
+    """
+
+
+app.run()
