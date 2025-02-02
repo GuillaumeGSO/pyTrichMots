d24b3b986d487d5024179e3a6ff79a3cee431a8c
Guillaume <>
2025-02-02 18:01:19 +0800

    Adapt to fastApi

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-# import json
-# import time
 from fastapi import FastAPI, Response
-# from seek_words import search_in_file, search_in_many_files
+from fastapi.responses import JSONResponse
+from seek_words import search_in_file, search_in_many_files
+import time
 
 app = FastAPI()
 # app.config["DEBUG"] = True
@@ -10,39 +10,35 @@ app = FastAPI()
 def home() -> Response:
     return Response("Server is running ! !")
 
-# @app.get('/simple/{lang}')
-# def simple(lang):
-#     nbcar = request.args.get('nbcar')
-#     lstcar = list(request.args.get('lstcar'))
-#     results = list(search_in_file(lang, nbcar, lstcar))
-#     response = jsonify(results)
-#     response.charset = "utf-8"
-#     response.data = json.dumps(results, ensure_ascii=False).encode('utf8')
-#     return response
-#     """
-#     testing URL
-#     http://127.0.0.1:5000/simple/fr?nbcar=8&lstcar=abcertyuiif
-#     http://127.0.0.1:5000/simple/en?nbcar=5&lstcar=merde
-#     """
-# @app.route('/multiple/{lang}>')
-# def multi(lang):
-#     cars = request.args.get('lstcar')
-#     start_time = time.time()
-#     results = list(search_in_many_files(lang, cars))
-#     end_time = time.time()
-    
-#     response = jsonify(results)
-#     response.charset = "utf-8"
-#     response.data = json.dumps({"total": len(results), "time": end_time - start_time, "results": results}, ensure_ascii=False).encode('utf8')
-#     return response
-    # http://127.0.0.1:5000/multiple/fr?lstcar=guillaume
-    # http://127.0.0.1:5000/multiple/en?lstcar=william
+@app.get('/simple/{lang}')
+def simple(lang:str, nbcar:str, lstcar:str) -> JSONResponse:
+    #     """
+    #     testing URL
+    #     http://127.0.0.1:5000/simple/fr?nbcar=8&lstcar=abcertyuiif
+    #     http://127.0.0.1:5000/simple/en?nbcar=5&lstcar=merde
+    #     """
+    start_time = time.time()
+    results = list(search_in_file(lang, nbcar, lstcar))
+    end_time = time.time()
+    return buildResponse(results, end_time - start_time)
+
+@app.get('/multiple/{lang}')
+def multi(lang:str, lstcar:str) -> JSONResponse:
+    #     """
+    #     testing URL
+    #     http://127.0.0.1:5000/multiple/fr?lstcar=guillaume
+    #     http://127.0.0.1:5000/multiple/en?lstcar=william
+    #     """
+    start_time = time.time()
+    results = list(search_in_many_files(lang, lstcar))
+    end_time = time.time()
+    return buildResponse(results, end_time - start_time)
 
 
-# def buildResponse(response, time):
-    # return json.dump({  
-    #     "total": len(response.data),
-    #     "result": response.data,
-    #     "time": time
-    #     })
+def buildResponse(results, time):
+    return JSONResponse({  
+        "time": time,
+        "total": len(results),
+        "result": results
+        })
 
